2dc667c1b48dbf913a1725d07d4a211a89ad333c
Matt E Patterson <matt.patterson@atlasobscura.com>
2022-11-04 10:34:52 -0500

    Working alpha

--- lib/stimulizer.rb
+++ lib/stimulizer.rb
@@ -1,8 +1,125 @@
 # frozen_string_literal: true
 
-require_relative "stimulizer/version"
+require "active_support/concern"
+require "dry-configurable"
+require "lucky_case"
 
+require_relative "stimulizer/version"
 module Stimulizer
+  extend Dry::Configurable
+  # extend ActiveSupport::Concern
+
   class Error < StandardError; end
-  # Your code goes here...
+
+  def self.included(klass)
+    klass.extend(ClassMethods)
+  end
+
+  module ClassMethods
+    @@stimulizer_opts = {output: :html_string}
+
+    def _stimulizer_opts
+      @@stimulizer_opts
+    end
+
+    def stimulize(hash)
+      @@stimulizer_opts.merge!(hash.deep_symbolize_keys)
+    end
+  end
+
+  def stimulus_controller
+    build_stimulus_controller
+  end
+
+  def stimulus(*args)
+    output = build_stimulus_hash(*args)
+    return output if _stimulizer_opts[:output] == :hash
+
+    raw(
+      output.to_a
+        .map { |k, v| %(#{k.to_s.strip}="#{v.to_s.strip}") }
+        .join(" ")
+        .squish
+        .strip
+    )
+  end
+
+  private
+
+  def _stimulizer_opts
+    self.class._stimulizer_opts
+  end
+
+  # EXAMPLE: My::Fancy::FooThing => "my--fancy--foo-thing"
+  def build_stimulus_controller
+    klass = self.class.name
+    klass = klass.gsub(_stimulizer_opts[:ignore_prefix], "") if _stimulizer_opts[:ignore_prefix].present?
+    klass.underscore.dasherize.gsub("/", "--")
+  end
+
+  # USAGE:
+  #   <%= stimulus(:controller)   <-- USES THE DEFAULT CONTROLLER
+  #   <%= stimulus(controller: "my-fancy-controller")
+  #   <%= stimulus(action: "click->doThing")
+  #   <%= stimulus(action: "click->doThing mouseup->otherThing")
+  #   <%= stimulus(action: "doThing") <-- default event for element
+  #   <%= stimulus(target: "button")
+  #   <%= stimulus(classes: {foo: "text-red-500", busy: "opacity-50 animate-spin"})
+  #   <%= stimulus(values: {url: "https://example.com"})
+  #   <%= stimulus(params: {foo: "bar", this_thing: "whatever"})
+  #
+  # ... or combine them:
+  #
+  #   <%= stimulus(:controller, target: "button", action: "click->doThing")
+  def build_stimulus_hash(default_controller = false, controller: nil, target: nil, action: nil, params: nil, values: nil, classes: nil)
+    raise ArgumentError(":controller specified, but blank") if controller&.blank?
+    raise ArgumentError(":target specified, but blank") if target&.blank?
+    raise ArgumentError(":action specified, but blank") if action&.blank?
+    raise ArgumentError(":params specified, but blank") if params&.blank?
+    raise ArgumentError(":values specified, but blank") if values&.blank?
+    raise ArgumentError(":classes specified, but blank") if classes&.blank?
+
+    {}.tap do |hash|
+      hash[:"data-controller"] = ""
+
+      if default_controller.to_s.downcase.to_sym == :controller
+        hash[:"data-controller"] += " #{stimulus_controller}"
+      end
+
+      hash[:"data-controller"] += " #{controller}" if controller
+
+      hash.delete(:"data-controller") if hash[:"data-controller"].blank?
+
+      if action
+        actions = action.squish.split(" ").map do |str|
+          next if str.blank?
+
+          arr = str.split("->")
+          event, function = arr.last.nil? ? [nil, arr.first] : ["#{arr.first}->", arr.last]
+
+          if function.include?("#")
+            "#{event}#{function}"
+          else
+            "#{event}#{stimulus_controller}##{function}"
+          end
+        end.compact
+
+        hash[:"data-action"] = actions.join(" ")
+      end
+
+      params&.each do |key, value|
+        hash[:"data-#{stimulus_controller}-#{LuckyCase.dash_case(key.to_s)}-param"] = value
+      end
+
+      values&.each do |key, value|
+        hash[:"data-#{stimulus_controller}-#{LuckyCase.dash_case(key.to_s)}-value"] = value
+      end
+
+      classes&.each do |key, value|
+        hash[:"data-#{stimulus_controller}-#{LuckyCase.dash_case(key.to_s)}-class"] = value
+      end
+
+      hash[:"data-#{stimulus_controller}-target"] = target if target
+    end
+  end
 end
--- stimulizer.gemspec
+++ stimulizer.gemspec
@@ -6,19 +6,18 @@ Gem::Specification.new do |spec|
   spec.name = "stimulizer"
   spec.version = Stimulizer::VERSION
   spec.authors = ["Matt E Patterson"]
-  spec.email = ["matt.patterson@atlasobscura.com"]
+  spec.email = ["mattp@digimonkey.com"]
 
-  spec.summary = "TODO: Write a short summary, because RubyGems requires one."
-  spec.description = "TODO: Write a longer description or delete this line."
-  spec.homepage = "TODO: Put your gem's website or public repo URL here."
+  spec.summary = "Stimulizer provides convenience methods for working with StimulusJS in views, templates, and components."
+  spec.homepage = "https://github.com/mepatterson/stimulizer"
   spec.license = "MIT"
   spec.required_ruby_version = ">= 2.6.0"
 
-  spec.metadata["allowed_push_host"] = "TODO: Set to your gem server 'https://example.com'"
+  spec.metadata["allowed_push_host"] = "https://rubygems.org/"
 
   spec.metadata["homepage_uri"] = spec.homepage
-  spec.metadata["source_code_uri"] = "TODO: Put your gem's public repo URL here."
-  spec.metadata["changelog_uri"] = "TODO: Put your gem's CHANGELOG.md URL here."
+  spec.metadata["source_code_uri"] = "https://github.com/mepatterson/stimulizer"
+  spec.metadata["changelog_uri"] = "https://github.com/mepatterson/stimulizer/blob/main/CHANGELOG.md"
 
   # Specify which files should be added to the gem when it is released.
   # The `git ls-files -z` loads the files in the RubyGem that have been added into git.
@@ -32,8 +31,10 @@ Gem::Specification.new do |spec|
   spec.require_paths = ["lib"]
 
   # Uncomment to register a new dependency of your gem
-  # spec.add_dependency "example-gem", "~> 1.0"
-
+  spec.add_dependency "dry-configurable", ">= 0.16.1"
+  spec.add_dependency "lucky_case", ">= 1.1.0"
+  spec.add_dependency "activesupport"
+  
   # For more information and examples about making a new gem, checkout our
   # guide at: https://bundler.io/guides/creating_gem.html
 end
